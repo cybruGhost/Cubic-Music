9169426138e901fcc59b44086931c881be0c1156
CYBERGHOST <stkibukah@gmail.com>
2025-10-20 23:30:42 +0300

    revertcode

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/home/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/home/HomeScreen.kt
@@ -12,25 +12,12 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -43,26 +30,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.Lifecycle
 import androidx.media3.common.util.UnstableApi
 import androidx.navigation.NavController
@@ -87,10 +58,11 @@ import kotlinx.coroutines.launch
 import me.knighthat.utils.Toaster
 import kotlin.system.exitProcess
 import it.fast4x.rimusic.LocalPlayerServiceBinder
+import androidx.compose.material3.MaterialTheme
 import android.webkit.DownloadListener
+import android.os.Environment
 import android.webkit.URLUtil
-import android.webkit.WebResourceError
-import kotlin.math.roundToInt
+import java.io.File
 
 @ExperimentalMaterial3Api
 @ExperimentalTextApi
@@ -149,112 +121,88 @@ fun HomeScreen(
         return
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Skeleton(
-            navController,
-            tabIndex,
-            onTabChanged,
-            miniPlayer,
-            navBarContent = { Item ->
-                if (enableQuickPicksPage)
-                    Item(0, stringResource(R.string.quick_picks), R.drawable.sparkles)
-                Item(1, stringResource(R.string.songs), R.drawable.musical_notes)
-                Item(2, stringResource(R.string.artists), R.drawable.people)
-                Item(3, stringResource(R.string.albums), R.drawable.album)
-                Item(4, stringResource(R.string.playlists), R.drawable.library)
-                Item(5, "Exportify", R.drawable.export_icon)
-            }
-        ) { currentTabIndex ->
-            saveableStateHolder.SaveableStateProvider(key = currentTabIndex) {
-                when (currentTabIndex) {
-                    0 -> HomeQuickPicks(
-                        onAlbumClick = {
-                            navController.navigate(route = "${NavRoutes.album.name}/$it")
-                        },
-                        onArtistClick = {
-                            navController.navigate(route = "${NavRoutes.artist.name}/$it")
-                        },
-                        onPlaylistClick = {
-                            navController.navigate(route = "${NavRoutes.playlist.name}/$it")
-                        },
-                        onSearchClick = {
-                            navController.navigate(NavRoutes.search.name)
-                        },
-                        onMoodClick = { mood ->
-                            navController.currentBackStackEntry?.savedStateHandle?.set("mood", mood.toUiMood())
-                            navController.navigate(NavRoutes.mood.name)
-                        },
-                        onSettingsClick = {
-                            navController.navigate(NavRoutes.settings.name)
-                        },
-                        navController = navController
-                    )
-
-                    1 -> HomeSongsScreen(navController)
-
-                    2 -> HomeArtists(
-                        onArtistClick = {
-                            navController.navigate(route = "${NavRoutes.artist.name}/${it.id}")
-                        },
-                        onSearchClick = {
-                            navController.navigate(NavRoutes.search.name)
-                        },
-                        onSettingsClick = {
-                            navController.navigate(NavRoutes.settings.name)
-                        }
-                    )
-
-                    3 -> HomeAlbums(
-                        navController = navController,
-                        onAlbumClick = {
-                            navController.navigate(route = "${NavRoutes.album.name}/${it.id}")
-                        },
-                        onSearchClick = {
-                            navController.navigate(NavRoutes.search.name)
-                        },
-                        onSettingsClick = {
-                            navController.navigate(NavRoutes.settings.name)
-                        }
-                    )
-
-                    4 -> HomeLibrary(
-                        onPlaylistClick = {
-                            navController.navigate(route = "${NavRoutes.localPlaylist.name}/${it.id}")
-                        },
-                        onSearchClick = {
-                            navController.navigate(NavRoutes.search.name)
-                        },
-                        onSettingsClick = {
-                            navController.navigate(NavRoutes.settings.name)
-                        }
-                    )
-                    
-                    5 -> ExportifyWebViewScreen()
-                }
-            }
+    Skeleton(
+        navController,
+        tabIndex,
+        onTabChanged,
+        miniPlayer,
+        navBarContent = { Item ->
+            if (enableQuickPicksPage)
+                Item(0, stringResource(R.string.quick_picks), R.drawable.sparkles)
+            Item(1, stringResource(R.string.songs), R.drawable.musical_notes)
+            Item(2, stringResource(R.string.artists), R.drawable.people)
+            Item(3, stringResource(R.string.albums), R.drawable.album)
+            Item(4, stringResource(R.string.playlists), R.drawable.library)
+            Item(5, "Exportify", R.drawable.export_icon)
         }
+    ) { currentTabIndex ->
+        saveableStateHolder.SaveableStateProvider(key = currentTabIndex) {
+            when (currentTabIndex) {
+                0 -> HomeQuickPicks(
+                    onAlbumClick = {
+                        navController.navigate(route = "${NavRoutes.album.name}/$it")
+                    },
+                    onArtistClick = {
+                        navController.navigate(route = "${NavRoutes.artist.name}/$it")
+                    },
+                    onPlaylistClick = {
+                        navController.navigate(route = "${NavRoutes.playlist.name}/$it")
+                    },
+                    onSearchClick = {
+                        navController.navigate(NavRoutes.search.name)
+                    },
+                    onMoodClick = { mood ->
+                        navController.currentBackStackEntry?.savedStateHandle?.set("mood", mood.toUiMood())
+                        navController.navigate(NavRoutes.mood.name)
+                    },
+                    onSettingsClick = {
+                        navController.navigate(NavRoutes.settings.name)
+                    },
+                    navController = navController
+                )
 
-        // DRAGGABLE DJ BUTTON - Smaller and movable
-        DraggableDJButton(
-            onTap = {
-                try {
-                    // Try multiple navigation approaches
-                    navController.navigate("DjVeda")
-                } catch (e: Exception) {
-                    // If DjVeda route doesn't work, try alternative
-                    try {
-                        navController.navigate("djveda") // lowercase
-                    } catch (e2: Exception) {
-                        android.util.Log.e("HomeScreen", "DJ navigation failed: ${e.message}")
-                        // Fallback to settings for testing
+                1 -> HomeSongsScreen(navController)
+
+                2 -> HomeArtists(
+                    onArtistClick = {
+                        navController.navigate(route = "${NavRoutes.artist.name}/${it.id}")
+                    },
+                    onSearchClick = {
+                        navController.navigate(NavRoutes.search.name)
+                    },
+                    onSettingsClick = {
                         navController.navigate(NavRoutes.settings.name)
                     }
-                }
-            },
-            modifier = Modifier.zIndex(100f)
-        )
+                )
+
+                3 -> HomeAlbums(
+                    navController = navController,
+                    onAlbumClick = {
+                        navController.navigate(route = "${NavRoutes.album.name}/${it.id}")
+                    },
+                    onSearchClick = {
+                        navController.navigate(NavRoutes.search.name)
+                    },
+                    onSettingsClick = {
+                        navController.navigate(NavRoutes.settings.name)
+                    }
+                )
+
+                4 -> HomeLibrary(
+                    onPlaylistClick = {
+                        navController.navigate(route = "${NavRoutes.localPlaylist.name}/${it.id}")
+                    },
+                    onSearchClick = {
+                        navController.navigate(NavRoutes.search.name)
+                    },
+                    onSettingsClick = {
+                        navController.navigate(NavRoutes.settings.name)
+                    }
+                )
+                
+                5 -> ExportifyWebViewScreen()
+            }
+        }
     }
 
     // Exit app when user uses back
@@ -282,175 +230,6 @@ fun HomeScreen(
     }
 }
 
-@Composable
-fun DraggableDJButton(
-    onTap: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    // Draggable state
-    var offsetX by remember { mutableStateOf(0f) }
-    var offsetY by remember { mutableStateOf(0f) }
-    
-    // Multiple animations for amazing effects
-    val infiniteTransition = rememberInfiniteTransition(label = "dj_button_animations")
-    
-    // Pulsing scale animation
-    val pulseScale by infiniteTransition.animateFloat(
-        initialValue = 1f,
-        targetValue = 1.05f, // Smaller pulse
-        animationSpec = infiniteRepeatable(
-            animation = tween(2000),
-            repeatMode = RepeatMode.Reverse
-        ),
-        label = "pulse_scale"
-    )
-    
-    // Rotation animation
-    val rotation by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(8000), // Slower rotation
-            repeatMode = RepeatMode.Restart
-        ),
-        label = "rotation"
-    )
-    
-    // Glow opacity animation
-    val glowAlpha by infiniteTransition.animateFloat(
-        initialValue = 0.2f,
-        targetValue = 0.6f, // Softer glow
-        animationSpec = infiniteRepeatable(
-            animation = tween(1500),
-            repeatMode = RepeatMode.Reverse
-        ),
-        label = "glow_alpha"
-    )
-
-    Box(
-        modifier = modifier
-            .offset { 
-                IntOffset(
-                    x = offsetX.roundToInt(),
-                    y = offsetY.roundToInt()
-                )
-            }
-            .size(44.dp) // Smaller size
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    change.consume()
-                    offsetX += dragAmount.x
-                    offsetY += dragAmount.y
-                }
-            }
-            .graphicsLayer {
-                scaleX = pulseScale
-                scaleY = pulseScale
-                rotationZ = rotation
-            }
-            .drawWithCache {
-                // Create gradient brush for the button
-                val gradientBrush = Brush.radialGradient(
-                    colors = listOf(
-                        Color(0xFF6200EE),
-                        Color(0xFF9C27B0),
-                        Color(0xFFE91E63)
-                    ),
-                    center = Offset(size.width / 2, size.height / 2),
-                    radius = size.width / 1.5f
-                )
-                
-                // Create glow brush
-                val glowBrush = Brush.radialGradient(
-                    colors = listOf(
-                        Color(0xFFFF00FF).copy(alpha = glowAlpha),
-                        Color(0xFF00FFFF).copy(alpha = glowAlpha * 0.5f),
-                        Color.Transparent
-                    ),
-                    center = Offset(size.width / 2, size.height / 2),
-                    radius = size.width * 1.1f
-                )
-                
-                onDrawWithContent {
-                    // Draw glow effect
-                    drawCircle(
-                        brush = glowBrush,
-                        radius = size.width / 1.8f,
-                        blendMode = BlendMode.Plus
-                    )
-                    
-                    // Draw main button with gradient
-                    drawCircle(
-                        brush = gradientBrush,
-                        radius = size.width / 2 - 3.dp.toPx()
-                    )
-                    
-                    // Draw outer ring
-                    drawCircle(
-                        color = Color.White.copy(alpha = 0.3f),
-                        radius = size.width / 2,
-                        style = Stroke(width = 1.5.dp.toPx())
-                    )
-                    
-                    // Draw content (icon)
-                    drawContent()
-                }
-            }
-            .shadow(
-                elevation = 12.dp,
-                shape = CircleShape,
-                clip = false
-            )
-            .clip(CircleShape)
-    ) {
-        FloatingActionButton(
-            onClick = onTap,
-            modifier = Modifier
-                .size(44.dp)
-                .scale(pulseScale),
-            shape = CircleShape,
-            containerColor = Color.Transparent, // Using custom draw instead
-            contentColor = Color.White
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                // Rotating icon with multiple effects
-                Icon(
-                    painter = painterResource(id = R.drawable.medical),
-                    contentDescription = "DJ Veda - Drag to move, Tap to open",
-                    modifier = Modifier
-                        .size(20.dp) // Smaller icon
-                        .rotate(rotation * -0.3f), // Less counter-rotation
-                    tint = Color.White
-                )
-                
-                // Tiny pulsing dot in center
-                Box(
-                    modifier = Modifier
-                        .size(2.dp)
-                        .align(Alignment.Center)
-                        .graphicsLayer {
-                            scaleX = 1f + (pulseScale - 1f) * 1.5f
-                            scaleY = 1f + (pulseScale - 1f) * 1.5f
-                            alpha = glowAlpha
-                        }
-                        .drawWithCache {
-                            onDrawBehind {
-                                drawCircle(
-                                    color = Color.Yellow.copy(alpha = glowAlpha),
-                                    radius = size.width / 2
-                                )
-                            }
-                        }
-                )
-            }
-        }
-    }
-}
-
-// Rest of the file remains the same (ExportifyWebViewScreen, etc.)
 class ExportifyWebInterface(private val context: android.content.Context) {
     @JavascriptInterface
     fun onDownloadRequested(url: String) {
@@ -509,6 +288,7 @@ fun ExportifyWebViewScreen() {
                     settings.apply {
                         javaScriptEnabled = true
                         domStorageEnabled = true
+                        databaseEnabled = true
                         setSupportMultipleWindows(true)
                         loadWithOverviewMode = true
                         useWideViewPort = true
@@ -597,19 +377,6 @@ fun ExportifyWebViewScreen() {
                             }
                         }
                         
-                        // FIXED: Updated deprecated onReceivedError method
-                        override fun onReceivedError(
-                            view: WebView?,
-                            request: WebResourceRequest?,
-                            error: WebResourceError?
-                        ) {
-                            super.onReceivedError(view, request, error)
-                            isLoading = false
-                            hasError = true
-                        }
-                        
-                        // For older Android versions, keep the deprecated method but suppress warning
-                        @Suppress("DEPRECATION")
                         override fun onReceivedError(
                             view: WebView?, 
                             errorCode: Int, 
@@ -679,4 +446,3 @@ fun ExportifyWebViewScreen() {
         }
     }
 }
-
