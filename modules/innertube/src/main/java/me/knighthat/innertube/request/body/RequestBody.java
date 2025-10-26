18b6cde3e290c997127216c3f4d86d7037934ebf
CYBERGHOST <stkibukah@gmail.com>
2025-10-26 19:08:03 +0300

    module change

--- modules/innertube/src/main/java/me/knighthat/innertube/request/body/RequestBody.java
+++
@@ -1,8 +0,0 @@
-package me.knighthat.innertube.request.body;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface RequestBody {
-
-    @NotNull Context getContext();
-}
--- modules/innertube/src/main/java/me/knighthat/innertube/response/Icon.java
+++
@@ -1,6 +0,0 @@
-package me.knighthat.innertube.response;
-
-public interface Icon {
-
-    String getIconType();
-}
