ba551622491de454bb0235951da9848c8de1ed8f
cybruGhost <stkibukah@gmail.com>
2026-01-16 22:21:11 +0300

    refactor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,36 +18,20 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Cubic-Music"
-
-/* =========================
-   Main App
-   ========================= */
 include(":composeApp")
-
-/* =========================
-   Extensions
-   ========================= */
+// Projects from extensions
 include(":oldtube")
 project(":oldtube").projectDir = file("extensions/innertube")
-
 include(":ktor-client-brotli")
 project(":ktor-client-brotli").projectDir = file("extensions/ktor-client-brotli")
-
 include(":kugou")
 project(":kugou").projectDir = file("extensions/kugou")
-
 include(":lrclib")
 project(":lrclib").projectDir = file("extensions/lrclib")
-
 include(":piped")
 project(":piped").projectDir = file("extensions/piped")
-
 include(":invidious")
 project(":invidious").projectDir = file("extensions/invidious")
-
-include(":genius")
-project(":genius").projectDir = file("extensions/genius")
-
- //submodules
+// Submodules
 include(":innertube")
 project(":innertube").projectDir = file("modules/innertube")
